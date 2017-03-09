1f9313da141d0d33faf5cdeb72c065b64c53f134
Taporpone <taporpone@gmail.com>
2017-03-09 19:31:13 +0100

    Init

---
+++ client/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import App from './App.js';
+
+/*const render = (Component) => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById('root')
+    );
+};*/
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// render(App);
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NewApp = require('./App').default;
+        render(newApp);
+    });
+}
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+var env = process.env.NODE_ENV || 'development';
+var path = require('path');
+
+var config = {
+  entry: [
+  './client/index.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename:'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader:"babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+};
+if (env === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeJsPlugin({
+      sourceMap: false
+    })
+  );
+}
+
+module.exports = config;
